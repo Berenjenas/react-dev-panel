67b8789cb60a03b6f4ed004236a95daacc39bca6
Emilio Dominguez <emilioadominguezb@gmail.com>
2025-07-26 15:22:31 -0300

    Added Zustand store initialization in Storybook preview decorator

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,14 +1,31 @@
-import type { Preview } from '@storybook/react-vite'
+import { useEffect } from "react";
+import type { Preview } from "@storybook/react-vite";
+
+import { useDevPanelStore } from "../src/utils/store";
 
 const preview: Preview = {
-  parameters: {
-    controls: {
-      matchers: {
-       color: /(background|color)$/i,
-       date: /Date$/i,
-      },
-    },
-  },
+	parameters: {
+		controls: {
+			matchers: {
+				color: /(background|color)$/i,
+				date: /Date$/i,
+			},
+		},
+	},
+	decorators: [
+		(Story) => {
+			useEffect(() => {
+				// Initialize the store state after Zustand hydration
+				const store = useDevPanelStore.getState();
+
+				store.setVisible(true);
+
+				console.log("[DevPanel] Setting visibility to true via decorator");
+			}, []);
+
+			return Story();
+		},
+	],
 };
 
 export default preview;
